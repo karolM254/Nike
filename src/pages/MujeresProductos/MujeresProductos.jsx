dafb502cd54d2d9dc938aca4508fdde8c28b67c8
Karol <montoyakr26@gmail.com>
2024-12-15 00:08:46 -0500

    Se agrego la logica del boton de buscar

--- src/pages/MujeresProductos/MujeresProductos.jsx
+++ src/pages/MujeresProductos/MujeresProductos.jsx
@@ -1,105 +1,97 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./MujeresProductos.css"; // Estilos específicos del componente
 
-const MujeresProductos = () => (
-  <div>
-    <main className="contenido-mujeres">
-      {/* Buscador */}
-      <div className="buscador-mujeres-productos">
-        <div className="buscador-mujeres">
-          <i className="ri-search-line"></i>
-          <input type="text" placeholder="Buscar" className="buscar-producto-mujer" />
-        </div>
-      </div>
+const MujeresProductos = () => {
+  const [searchTerm, setSearchTerm] = useState(""); // Estado para el buscador
 
-      {/* Grid de contenido */}
-      <div className="grid-contenido-mujeres">
-        {/* Fila 1 de productos */}
-        <div className="col col-1">
-          <Link to={`/producto/1`}>
-            <img
-              src="/assets/679005-500-500.webp"
-              alt="AIR JORDAN 1 HIGH METHOD OF MAKE"
+  // Lista de productos
+  const productos = [
+    {
+      id: 1,
+      nombre: "AIR JORDAN 1 HIGH METHOD OF MAKE",
+      descripcion: "Calzado para mujer",
+      precio: "$1.004.950",
+      img: "/assets/679005-500-500.webp",
+    },
+    {
+      id: 2,
+      nombre: "NIKE VICTORI ONE",
+      descripcion: "Chancla para mujer",
+      precio: "$199.950",
+      img: "/assets/677567-500-500.webp",
+    },
+    {
+      id: 3,
+      nombre: "NIKE AIR MAX SC",
+      descripcion: "Calzado para mujer",
+      precio: "$564.950",
+      img: "/assets/678301-500-500.webp",
+    },
+    {
+      id: 4,
+      nombre: "W NIKE ZOOM BELLA 6 PRM",
+      descripcion: "Calzado para mujer",
+      precio: "$524.950",
+      img: "/assets/678274-500-500.webp",
+    },
+    {
+      id: 5,
+      nombre: "AIR JORDAN 1 LOW SE",
+      descripcion: "Calzado para mujer",
+      precio: "$834.950",
+      img: "/assets/678936-500-500.webp",
+    },
+    {
+      id: 6,
+      nombre: "NIKE COURT VISION ALTA",
+      descripcion: "Calzado para mujer",
+      precio: "$254.950",
+      img: "/assets/677786-500-500.webp",
+    },
+  ];
+
+  // Filtrar productos por el nombre del zapato
+  const productosFiltrados = productos.filter((producto) =>
+    producto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <main className="contenido-mujeres">
+        {/* Buscador */}
+        <div className="buscador-mujeres-productos">
+          <div className="buscador-mujeres">
+            <i className="ri-search-line"></i>
+            <input
+              type="text"
+              placeholder="Buscar"
+              className="buscar-producto-mujer"
+              value={searchTerm} // Valor del input
+              onChange={(e) => setSearchTerm(e.target.value)} // Actualizar estado
             />
-          </Link>
-          <h4>AIR JORDAN 1 HIGH METHOD OF MAKE</h4>
-          <div className="precio-mujeres">
-            <h5>Calzado para mujer</h5>
-            <p>$1.004.950</p>
-          </div>
-        </div>
-        <div className="col col-1">
-          <Link to={`/producto/2`}>
-            <img
-              src="/assets/677567-500-500.webp"
-              alt="NIKE VICTORI ONE"
-            />
-          </Link>
-          <h4>NIKE VICTORI ONE</h4>
-          <div className="precio-mujeres">
-            <h5>Chancla para mujer</h5>
-            <p>$199.950</p>
-          </div>
-        </div>
-        <div className="col col-1">
-          <Link to={`/producto/3`}>
-            <img
-              src="/assets/678301-500-500.webp"
-              alt="NIKE AIR MAX SC"
-            />
-          </Link>
-          <h4>NIKE AIR MAX SC</h4>
-          <div className="precio-mujeres">
-            <h5>Calzado para mujer</h5>
-            <p>$564.950</p>
           </div>
         </div>
 
-        {/* Fila 2 de productos */}
-        <div className="col col-2">
-          <Link to={`/producto/4`}>
-            <img
-              src="/assets/678274-500-500.webp"
-              alt="W NIKE ZOOM BELLA 6 PRM"
-            />
-          </Link>
-          <h4>W NIKE ZOOM BELLA 6 PRM</h4>
-          <div className="precio-mujeres">
-            <h5>Calzado para mujer</h5>
-            <p>$524.950</p>
-          </div>
+        {/* Grid de contenido */}
+        <div className="grid-contenido-mujeres">
+          {productosFiltrados.map((producto) => (
+            <div className="col" key={producto.id}>
+              <Link to={`/producto/${producto.id}`}>
+                <img src={producto.img} alt={producto.nombre} />
+              </Link>
+              <h4>{producto.nombre}</h4>
+              <div className="precio-mujeres">
+                <h5>{producto.descripcion}</h5>
+                <p>{producto.precio}</p>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="col col-2">
-          <Link to={`/producto/5`}>
-            <img
-              src="/assets/678936-500-500.webp"
-              alt="AIR JORDAN 1 LOW SE"
-            />
-          </Link>
-          <h4>AIR JORDAN 1 LOW SE</h4>
-          <div className="precio-mujeres">
-            <h5>Calzado para mujer</h5>
-            <p>$834.950</p>
-          </div>
-        </div>
-        <div className="col col-2">
-          <Link to={`/producto/6`}>
-            <img
-              src="/assets/677786-500-500.webp"
-              alt="NIKE COURT VISION ALTA"
-            />
-          </Link>
-          <h4>NIKE COURT VISION ALTA</h4>
-          <div className="precio-mujeres">
-            <h5>Calzado para mujer</h5>
-            <p>$254.950</p>
-          </div>
-        </div>
-      </div>
-      <div className="mas-productos-mujeres">
-      </div>
-    </main>
-  </div>
-);
+        <div className="mas-productos-mujeres"></div>
+      </main>
+    </div>
+  );
+};
 
 export default MujeresProductos;
