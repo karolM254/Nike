368a8263084b607a01f79a9e881894dcb243deeb
IkerGrisales <ikerggrisales@gmail.com>
2024-12-16 00:06:24 -0500

    Actualización de formulario atención al cliente.

--- src/pages/reseñas/Reseñas.jsx
+++ src/pages/reseñas/Reseñas.jsx
@@ -6,16 +6,16 @@ function Reseñas() {
   return (
     <div className="reseñas-container">
       <h2 className="title" >ATENCIÓN AL CLIENTE</h2>
-      <div className="line"></div>
+      <div className="linea-resenas"></div>
       <div>
         <form className='form-client-atention' action="#" method="post">
           <div className="form-labels ">
               <label htmlFor="nombre">Nombre Completo</label>
-              <input type="text" id="nombre" name="nombre" class="input-text" required />
+              <input type="text" id="nombre" name="nombre" className="input-text" required />
               <label htmlFor="apellido">Teléfono</label>
-              <input type="text" id="apellido" name="apellido" class="input-text" required />
+              <input type="text" id="apellido" name="apellido" className="input-text" required />
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" class="input-text" required />
+              <input type="email" id="email" name="email" className="input-text" required />
           </div>
           <label htmlFor="comentario">
             <strong>Comentario / Descripción del problema</strong>
